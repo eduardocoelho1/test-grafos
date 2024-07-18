94099e308facc5b4bdcd441ece8f4f472ab91d69
Eduardo Coelho <147324833+eduardocoelho1@users.noreply.github.com>
2024-07-18 14:43:02 -0300

    Update listaadj.cpp

--- listaadj.cpp
+++ listaadj.cpp
@@ -4,95 +4,73 @@
 #include <iostream>
 using namespace std;
 
-class TreeNode {
-    friend class Tree;
-    private:
-        int value;
-        vector<TreeNode*> children;
-    public:
-        TreeNode(int val) { value = val; }
-};
-
-class Tree {
-    private:
-        TreeNode* root;
-    public:
-        Tree() { root = nullptr; }
-        void insert(int val, int parent) {
-            TreeNode* newNode = new TreeNode(val);
-        }
-
-};
-
 class Graph {
-    private:
-        set<int>* adjList;
-        int* parents;
-        enum NodeColor { WHITE, GRAY, BLACK };
-        NodeColor* colors;
-        int size;
-    public:
-        Graph(set<pair<int, int>> pairs, int n) {
-            adjList = new set<int>[n];
-            for (pair<int,int> pair: pairs) {
-                adjList[pair.first].insert(pair.second);
-                adjList[pair.second].insert(pair.first);
-            }
-            parents = new int[n];
-            colors = new NodeColor[n];
-            size = n;
+private:
+    set<int>* adjList;
+    int* parents;
+    enum VertexColor { WHITE, GRAY, BLACK };
+    VertexColor* colors;
+    int size;
+public:
+    Graph(set<pair<int, int>> pairs, int n) {
+        adjList = new set<int>[n];
+        for (pair<int,int> pair: pairs) {
+            adjList[pair.first].insert(pair.second);
+            adjList[pair.second].insert(pair.first);
         }
-        ~Graph() {
-            delete [] adjList;
-            delete [] parents;
-            delete [] colors;
-        }
-        void bfs(int origin) {
-            queue<int> discovered;
-            discovered.push(origin);
-            parents[origin] = -1;
-            while (!discovered.empty()) {
-                int vertex = discovered.front();
-                discovered.pop();
-                colors[vertex] = GRAY;
-                for (int elem: adjList[vertex]) {
-                    if (colors[elem] == WHITE) {
-                        parents[elem] = vertex;
-                        colors[elem] = GRAY;
-                        discovered.push(elem);
-                    }
+        parents = new int[n];
+        colors = new VertexColor[n];
+        size = n;
+    }
+    ~Graph() {
+        delete [] adjList;
+        delete [] parents;
+        delete [] colors;
+    }
+    void bfs(int origin) {
+        queue<int> discovered;
+        discovered.push(origin);
+        parents[origin] = -1;
+        while (!discovered.empty()) {
+            int vertex = discovered.front();
+            discovered.pop();
+            colors[vertex] = GRAY;
+            for (int elem: adjList[vertex]) {
+                if (colors[elem] == WHITE) {
+                    parents[elem] = vertex;
+                    colors[elem] = GRAY;
+                    discovered.push(elem);
                 }
-                colors[vertex] = BLACK;
             }
-            for (int i = 1; i < size; i++) {
-                cout << "parents[" << i << "]: " << parents[i] << endl;
-            }
-            for (int i = 1; i < size; i++) {
-                cout << "colors[" << i << "]: " << colors[i] << endl;
-            }
+            colors[vertex] = BLACK;
         }
-        void print() {
-            for (int i = 1; i < size; i++) {
-                cout << "adjList[" << i << "] = ";
-                for (int elem: adjList[i]) {
-                    cout << elem << " ";
-                }
-                cout << endl;
-            }
+        for (int i = 1; i < size; i++) {
+            cout << "parents[" << i << "]: " << parents[i] << endl;
         }
-        bool isEulerian() {
-            bool result = true;
-            int i = 0;
-            while (result and i < size) {
-                if (adjList[i].size() % 2 != 0)
-                    result = false;
-                i++;
-            }
-            return result;
+        for (int i = 1; i < size; i++) {
+            cout << "colors[" << i << "]: " << colors[i] << endl;
         }
+    }
+    void print() {
+        for (int i = 1; i < size; i++) {
+            cout << "adjList[" << i << "] = ";
+            for (int elem: adjList[i]) {
+                cout << elem << " ";
+            }
+            cout << endl;
+        }
+    }
+    bool isEulerian() {
+        bool result = true;
+        int i = 0;
+        while (result and i < size) {
+            if (adjList[i].size() % 2 != 0)
+                result = false;
+            i++;
+        }
+        return result;
+    }
 };
-
-
 
 int main() {
     int n, u, v;
@@ -102,12 +80,12 @@ int main() {
         pairs.insert(pair(u,v));
     }
     Graph g(pairs, n+1);
-    g.bfs(1);
     g.print();
+    cout << '\n';
+    g.bfs(1);
+    cout << '\n';
     if (g.isEulerian())
         cout << "Sim\n";
     else
         cout << "Não\n";
 }
-
-
